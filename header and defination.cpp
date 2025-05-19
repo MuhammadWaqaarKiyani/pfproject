38b10c4b3abffc25cea986fa2c01fa3f775c422b
Asnahasan90 <asnajawad1527@gmail.com>
2025-05-19 12:14:38 +0500

    Update header and defination.cpp

--- header and defination.cpp
+++ header and defination.cpp
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #define MAX_ROOMMATES 10
-#define MAX_EXPENSES 50/
+#define MAX_EXPENSES 50
 #define NAME_LEN 50
 #define USER_FILE "users.txt"
 #define DATA_FILE "expenses.txt"
